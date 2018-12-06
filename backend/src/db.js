af1bfaecb21faa10bf74135ca5e3859a24144ce1
Misha Marinenko <a0x77@icloud.com>
2018-12-06 20:41:38 +0200

    #5 : База

---
+++ backend/src/db.js
@@ -0,0 +1,6 @@
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const uuidv1 = require('uuid/v1');
+const adapter = new FileSync('db/db.json');
+const db = low(adapter);
+
